f477bb74707046da9a7f62036ed08ce0001cb739
Your Name <you@example.com>
2025-01-25 21:31:59 +0100

    Correzione nella gestione dei caratteri multibyte
    Il precedente metodo che permetteva la stampa del testo decifrato non
    gestiva correttamente i casi in cui c'erano caratteri multibyte. Per
    questo, il metodo è stato modificato in modo che, grazie ad un and, sia in
    grado di verificare se il byte che sta analizzando fa parte di carattere
    multibyte, questo perché i caratteri multibyte hanno come primo bit, un
    valore uguale ad 1, quindi è possibile controllare quel bit e scorrere
    finchè il carattere multibyte non finisce. Il ciclo while continua finchè
    ci sono caratteri multibyte che poi vengono convertiti in stringa grazie
    al costruttore.

--- Cifrario/src/main/java/org/example/cifrario/app/Cifratore.java
+++ Cifrario/src/main/java/org/example/cifrario/app/Cifratore.java
@@ -31,15 +31,34 @@ public class Cifratore {
 
     }
 
-    private void stampaTestoDecifrato(byte[] testoCifrato){
+    private void stampaTestoDecifrato(byte[] testoCifrato) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < testoCifrato.length; i++) {
-            stringBuilder.append( (char) testoCifrato[i]);
+            // I caratteri che sono formato da più byte hanno il valore del primo bit assegnato a 1 quindi è possibile
+            // fare l'and tra il byte e un valore 0x80 che corrisponde ad un byte che come primo bit un valore uguale a 1
+            // e se l'and è uguale a 0x80, significa che il primo bit è uguale a 1 e pertanto è multibyte
+
+            if ((testoCifrato[i] & 0x80) == 0x80) {
+                int j = i;
+                // Continua a leggere i byte successivi per formare il carattere completo
+                while (j < testoCifrato.length && (testoCifrato[j] & 0x80) == 0x80) {
+                    j++;
+                }
+                // Ora, possiamo trattare la sequenza di byte dal punto di inizio fino a j
+                byte[] multibyteChar = new byte[j - i];
+                System.arraycopy(testoCifrato, i, multibyteChar, 0, j - i); //copia dell'array
+                // Converte la sequenza di byte in un carattere
+                String decodedChar = new String(multibyteChar, StandardCharsets.UTF_8);
+                stringBuilder.append(decodedChar);
+                i = j - 1;  // Avanzare l'indice per saltare i byte già processati
+            } else {
+                // Se il byte è un singolo byte (ASCII standard)
+                stringBuilder.append((char) testoCifrato[i]);
+            }
         }
-
-        System.out.println(stringBuilder);
-
+        System.out.println("Testo decifrato: " + stringBuilder);
     }
+
 
     private byte[] generateKey(int size){
         SecureRandom secureRandom = new SecureRandom();
